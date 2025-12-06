8df123ec67bb10f72d2fbbdf687692b2ff1357b9
Fengy2001 <99842837+Fengy2001@users.noreply.github.com>
2025-12-06 11:11:38 -0700

    nextjs config adjustment for docker build

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,12 @@ import type { NextConfig } from "next";
 import { output } from "three/tsl";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       type: 'asset/source'
     });
-
-    config.module.export = {
-      output: "standalone",
-    };
 
     return config;
   }
